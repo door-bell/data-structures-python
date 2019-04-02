0ee7e1e941227e44212f34fd46658d41a44ad272
door-bell <socaltdb@gmail.com>
2019-04-02 09:30:42 -0700

    Add deletion tests

--- test_bst.py
+++ test_bst.py
@@ -10,6 +10,28 @@ class TestInsertion(unittest.TestCase):
         tree.insert(2)
         self.assertTrue(tree.contains(1))
         self.assertTrue(tree.contains(2))
+    def test_deletion(self):
+        tree = BST()
+        # Root
+        tree.insert(10)
+        # Left subtree
+        tree.insert(5)
+        tree.insert(4)
+        tree.insert(6)
+        # Right subtree
+        tree.insert(15)
+        tree.insert(14)
+        tree.insert(13)
+
+        # Test deltion of node with 2 children
+        tree.delete(15)
+        self.assertFalse(tree.contains(15))
+        self.assertTrue(tree.contains(5))
+        self.assertTrue(tree.contains(4))
+        self.assertTrue(tree.contains(6))
+        self.assertTrue(tree.contains(14))
+        self.assertTrue(tree.contains(13))
+
 
 if __name__ == '__main__':
     unittest.main()
